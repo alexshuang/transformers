c679921fdfaa97c286e80ac4b5206067549f12fe
Huang Shuang <nikshuang@sina.com>
2020-07-28 23:22:59 +0000

    update script

--- benchmarks/run_squad.sh
+++ benchmarks/run_squad.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 OUT_DIR=${1:-out}
+TMP_DIR=$OUT_DIR/tmp
 SQUAD_DIR=/data/squad
 MODEL_NAME=bert-large-uncased
 TRAIN_FILE=train-v1.1.json
@@ -12,7 +13,7 @@ BS=${3:-4}
 SEQ_LEN=${4:-512}
 
 #rm -rf $OUT_DIR
-mkdir -p $OUT_DIR
+mkdir -p $OUT_DIR $TMP_DIR
 
 if [ ! -f $SQUAD_DIR/$TRAIN_FILE ]; then
 	wget -P $SQUAD_DIR https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
@@ -38,27 +39,26 @@ CMD="python3.6 ../examples/run_squad.py \
 set -e
 
 # end2end perf
-$CMD --result_dir $OUT_DIR --max_steps $STEPS
+$CMD --result_dir $OUT_DIR --max_steps $STEPS | tee $TMP_DIR/run.log
 
 # record kernels
-export ROCBLAS_LAYER=2
+export ROCBLAS_LAYER=6
 export ROCBLAS_LOG_BENCH_PATH=${OUT_DIR}/rocblas_bench.csv
+export ROCBLAS_LOG_PROFILE_PATH=${OUT_DIR}/rocblas_config.json
 rm -f ${ROCBLAS_LOG_BENCH_PATH}
-
+rm -f ${ROCBLAS_LOG_PROFILE_PATH}
 echo "pmc: FetchSize L2CacheHit" > input.txt
-/opt/rocm/bin/rocprof -i input.txt --obj-tracking on --timestamp on --stats -o ${OUT_DIR}/kernel_prof.csv \
+/opt/rocm/bin/rocprof -i input.txt --obj-tracking on --timestamp on --stats -o ${TMP_DIR}/kernel_prof.csv \
 $CMD --max_steps 1
-rm -f ${OUT_DIR}/*.db ${OUT_DIR}/*.json ${OUT_DIR}/*.txt
 
-sed -n '/Cijk_A/p' ${OUT_DIR}/kernel_prof.csv | awk -F, '{print $2}' > ${OUT_DIR}/kernel_name.csv
+sed -n '/Cijk_A/p' ${TMP_DIR}/kernel_prof.csv > ${OUT_DIR}/kernel_prof.csv
 
 # rocblas-bench
 TOOL=/root/rocblas/build/release/clients/staging/rocblas-bench
 if [ ! -e rocblas-bench ]; then
 	ln -s ${TOOL} .
 fi
-
 unset ROCBLAS_LAYER
-sh $ROCBLAS_LOG_BENCH_PATH | tee /tmp/rb_res.txt
-sed -E -n '/(^N,|^T,)/p' /tmp/rb_res.txt > $OUT_DIR/rocblas_bench_res.csv
+sh $ROCBLAS_LOG_BENCH_PATH 2>&1 > $TMP_DIR/rb_res.txt | tee rocblas_bench.log
+sed -E -n '/(^N,|^T,)/p' $TMP_DIR/rb_res.txt > $OUT_DIR/rocblas_bench_res.csv
 echo "File $OUT_DIR/rocblas_bench_res.csv is generated."
